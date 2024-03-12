471b8914964880b0bf7c7a4d81713506f3ab1cb7
REEEee42069 <118033335+REEEee42069@users.noreply.github.com>
2024-03-12 12:39:54 -0500

    Update CardDeck.js

--- server/CardDeck.js
+++ server/CardDeck.js
@@ -1,4 +1,4 @@
-const cards = require('./ClassicPack.js');
+const cards = require('./FamilyPack.js');
 const shuffleArray = require("./Utility.js");
 
 // CardDeck contains all cards, black and white
